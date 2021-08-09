dcb8b42b3b1fa6a029a93553c2d9baefdc60ff12
Jean Carlos soncco lupa <jsonccolu@unsa.edu.pe>
2021-08-09 02:04:22 -0500

    update files

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
-from api.views import RegisterView,DetailPrediction
+from api.views import RegisterView, DetailPrediction, DataListView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='Register'),
     path('detail/<int:pk>/',DetailPrediction.as_view(),name='detail prediction'),
+    path('',DataListView.as_view(), name='list'),
 ]
--- api/views.py
+++ api/views.py
@@ -10,6 +10,10 @@ from api.models import DataFormulario, DataPrediction
 from rest_framework.views import APIView
 from django.http import Http404
 
+from os import getcwd, scandir
+from os.path import abspath
+import time
+
 class RegisterView(generics.GenericAPIView):
     serializer_class = DataSerializer
 
@@ -22,18 +26,36 @@ class RegisterView(generics.GenericAPIView):
         task.setDaemon(True)
         task.start()
 
+        task1 = threading.Thread(target=loadImageToDataBase,args=[user])
+        task1.setDaemon(True)
+        task1.start()
 
         return Response(serializer.data, status=status.HTTP_200_OK)
 
+class DataListView(APIView):
+    def get(self, request, *args, **kwargs):
+        data = DataFormulario.objects.all()
+        data_serializer = DataSerializer(data, many=True)
+
+        return Response(data_serializer.data, status=status.HTTP_200_OK)
+
+def ls(ruta = getcwd()):
+    return [arch.name for arch in scandir(ruta) if arch.is_file()]
 
 def predictionYolo(user):
     #output = os.popen('flutter doctor {0}'.format('file')).read()
     #print(output)
-    f = File(open('yolo/input.png'),'rb')
+    #f = File(open('yolo/input.png'),'rb')
+    print("")
 
-    datapred = DataPrediction(user=user)
-    datapred.image = 'input.png'
-    datapred.save()
+def loadImageToDataBase(user):
+    time.sleep(0.3)
+    if os.listdir('yolo/imagen/'):
+        files = ls('yolo/imagen')
+        for _file in files:
+            datapred = DataPrediction(user=user)
+            datapred.image = 'imagen/' + _file
+            datapred.save()
 
 class DetailPrediction(APIView):
     #serializer_class = DataViewSerializer
