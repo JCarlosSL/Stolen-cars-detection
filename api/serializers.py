f6d5ff931b2f55d1f20967c454fe27d5633ae0d1
Jean Carlos soncco lupa <jsonccolu@unsa.edu.pe>
2021-08-10 10:10:57 -0500

    Labels deepstream

--- api/serializers.py
+++ api/serializers.py
@@ -1,6 +1,54 @@
 from rest_framework import serializers
 from api.models import DataFormulario, DataPrediction
 
+color = {
+    'black' : 'black;;;;;;;;;;;',
+    'blue' : ';blue;;;;;;;;;;',
+    'brown' : ';;brown;;;;;;;;;',
+    'gold' : ';;;gold;;;;;;;;',
+    'green' : ';;;;green;;;;;;;',
+    'grey' : ';;;;;grey;;;;;;',
+    'maroon' : ';;;;;;maroon;;;;;',
+    'orange' : ';;;;;;;orange;;;;',
+    'red' : ';;;;;;;;red;;;',
+    'silver' : ';;;;;;;;;silver;;',
+    'white' : ';;;;;;;;;;white;',
+    'yellow' : ';;;;;;;;;;;yellow',
+}
+
+marca = {
+    'acura' : 'acura;;;;;;;;;;;;;;;;;;;',
+    'audi' : ';audi;;;;;;;;;;;;;;;;;;',
+    'bmw' : ';;bmw;;;;;;;;;;;;;;;;;',
+    'chevrolet' : ';;;chevrolet;;;;;;;;;;;;;;;;',
+    'chrysler' : ';;;;chrysler;;;;;;;;;;;;;;;',
+    'dodge' : ';;;;;dodge;;;;;;;;;;;;;;',
+    'ford' : ';;;;;;ford;;;;;;;;;;;;;',
+    'gmc' : ';;;;;;;gmc;;;;;;;;;;;;',
+    'honda' : ';;;;;;;;honda;;;;;;;;;;;',
+    'hyundai' : ';;;;;;;;;hyundai;;;;;;;;;;',
+    'infiniti' : ';;;;;;;;;;infiniti;;;;;;;;;',
+    'jeep' : ';;;;;;;;;;;jeep;;;;;;;;',
+    'kia' : ';;;;;;;;;;;;kia;;;;;;;',
+    'lexus' : ';;;;;;;;;;;;;lexus;;;;;;',
+    'mazda' : ';;;;;;;;;;;;;;mazda;;;;;',
+    'mercedes' : ';;;;;;;;;;;;;;;mercedes;;;;',
+    'nissan' : ';;;;;;;;;;;;;;;;nissan;;;',
+    'subaru' : ';;;;;;;;;;;;;;;;;subaru;;',
+    'toyota' : ';;;;;;;;;;;;;;;;;;toyota;',
+    'volkswagen' : ';;;;;;;;;;;;;;;;;;;volkswagen',
+
+}
+
+tipo = {
+    'couple' : 'coupe;;;;;',
+    'largevehicle' : ';largevehicle;;;;',
+    'sedan' : ';;sedan;;;',
+    'suv' : ';;;suv;;',
+    'truck' : ';;;;truck;',
+    'van' : ';;;;;van',
+}
+
 class DataSerializer(serializers.ModelSerializer):
     email = serializers.EmailField()
     class Meta:
@@ -11,13 +59,13 @@ class DataSerializer(serializers.ModelSerializer):
         data = DataFormulario(**validated_data)
         data.save()
         f = open('yolo/files/Secondary_CarColor/labels.txt','w')
-        f.write(validated_data['color'])
+        f.write(color[validated_data['color']])
         f.close()
         f = open('yolo/files/Secondary_CarMake/labels.txt','w')
-        f.write(validated_data['marca'])
+        f.write(marca[validated_data['marca']])
         f.close()
         f = open('yolo/files/Secondary_VehicleType/labels.txt','w')
-        f.write(validated_data['tipo'])
+        f.write(tipo[validated_data['tipo']])
         f.close()
         return data
 
